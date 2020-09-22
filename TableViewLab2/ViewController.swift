d5c982943d81f944bbe17c5e45e233746f09844d
Pretty-Villy <63590833+Pretty-Villy@users.noreply.github.com>
2020-09-22 08:36:53 +0600

    Initial Commit

---
+++ TableViewLab2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewLab2
+//
+//  Created by Darkhan Zhapparov on 9/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
